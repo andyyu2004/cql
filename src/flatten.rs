12b1b01fec7e30cca8682be5c6e67b84714d1919
Andy Yu <andyyu2004@gmail.com>
2024-10-30 10:09:59 +1300

    tricky

--- src/flatten.rs
+++ src/flatten.rs
@@ -1,13 +1,23 @@
 use serde::Serializer;
 
-struct DotFlattened<S> {
+pub struct DotFlattened<S> {
     inner: S,
+    state: State,
 }
 
 impl<S> DotFlattened<S> {
     fn new(inner: S) -> Self {
-        Self { inner }
+        Self {
+            inner,
+            state: State::Top,
+        }
     }
+}
+
+enum State {
+    Top,
+    InMap,
+    InList,
 }
 
 impl<S: Serializer> Serializer for DotFlattened<S> {
@@ -62,46 +72,46 @@ impl<S: Serializer> Serializer for DotFlattened<S> {
     }
 
     fn serialize_u64(self, v: u64) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        self.inner.serialize_u64(v)
     }
 
     fn serialize_f32(self, v: f32) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        self.inner.serialize_f32(v)
     }
 
     fn serialize_f64(self, v: f64) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        self.inner.serialize_f64(v)
     }
 
     fn serialize_char(self, v: char) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        self.inner.serialize_char(v)
     }
 
     fn serialize_str(self, v: &str) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        self.inner.serialize_str(v)
     }
 
     fn serialize_bytes(self, v: &[u8]) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        self.inner.serialize_bytes(v)
     }
 
     fn serialize_none(self) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        self.inner.serialize_none()
     }
 
     fn serialize_some<T>(self, value: &T) -> Result<Self::Ok, Self::Error>
     where
         T: ?Sized + serde::Serialize,
     {
-        todo!()
+        self.inner.serialize_some(value)
     }
 
     fn serialize_unit(self) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        self.inner.serialize_unit()
     }
 
     fn serialize_unit_struct(self, name: &'static str) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        self.inner.serialize_unit_struct(name)
     }
 
     fn serialize_unit_variant(
@@ -110,7 +120,8 @@ impl<S: Serializer> Serializer for DotFlattened<S> {
         variant_index: u32,
         variant: &'static str,
     ) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        self.inner
+            .serialize_unit_variant(name, variant_index, variant)
     }
 
     fn serialize_newtype_struct<T>(
@@ -121,7 +132,7 @@ impl<S: Serializer> Serializer for DotFlattened<S> {
     where
         T: ?Sized + serde::Serialize,
     {
-        todo!()
+        self.inner.serialize_newtype_struct(name, value)
     }
 
     fn serialize_newtype_variant<T>(
@@ -134,11 +145,12 @@ impl<S: Serializer> Serializer for DotFlattened<S> {
     where
         T: ?Sized + serde::Serialize,
     {
-        todo!()
+        self.inner
+            .serialize_newtype_variant(name, variant_index, variant, value)
     }
 
     fn serialize_seq(self, len: Option<usize>) -> Result<Self::SerializeSeq, Self::Error> {
-        todo!()
+        Ok(&mut self)
     }
 
     fn serialize_tuple(self, len: usize) -> Result<Self::SerializeTuple, Self::Error> {
@@ -172,7 +184,7 @@ impl<S: Serializer> Serializer for DotFlattened<S> {
         name: &'static str,
         len: usize,
     ) -> Result<Self::SerializeStruct, Self::Error> {
-        todo!()
+        self.inner.serialize_struct(name, len)
     }
 
     fn serialize_struct_variant(
@@ -182,7 +194,8 @@ impl<S: Serializer> Serializer for DotFlattened<S> {
         variant: &'static str,
         len: usize,
     ) -> Result<Self::SerializeStructVariant, Self::Error> {
-        todo!()
+        self.inner
+            .serialize_struct_variant(name, variant_index, variant, len)
     }
 }
 
